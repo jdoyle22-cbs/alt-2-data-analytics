90f0fe76e7b55779044ba0e3d782cbf4e527f748
jdoyle22-cbs <jamesdoyle22@student.cbsenniscorthy.ie>
2026-02-25 12:37:36 +0000

    feat: cli filename argument

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import download_data from download
+import sys
 
+# Handle CLI arguments
+filename: str = sys.argv[1] if sys.argv[1] else "movies" # Default to movies if custom filename not specified
+
+# Welcome message
 print(
 """
    _   _  _____ ___ 
@@ -14,4 +19,18 @@ Data Analytics
 """
 )
 
-download_data() # Prompt user to redownload data if they wish
+download_data(filename) # Prompt user to redownload data if they wish
+
+def main_loop(file: File):
+   print("TBD")
+
+print("Reading data file...")
+
+# Attempt to write to file, handling errors
+try:
+   with open(f"{filename if filename else "movies"}.csv", "w") as file:
+      main_loop(file)
+except IOError:
+   print("The file could not be read. Is it being used by another program?")
+except Exception as e:
+   print("The following error occurred: ", e)
