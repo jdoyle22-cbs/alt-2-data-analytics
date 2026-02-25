95f6431915d493ccb8fa8308c125aef1160f59a4
jdoyle22-cbs <jamesdoyle22@student.cbsenniscorthy.ie>
2026-02-25 12:22:45 +0000

    feat: initial base of program

---
+++ main.py
@@ -0,0 +1,17 @@
+import download_data from download
+
+print(
+"""
+   _   _  _____ ___ 
+  /_\ | ||_   _|_  )
+ / _ \| |__| |  / / 
+/_/ \_\____|_| /___|
+
+========================================
+Data Analytics
+========================================
+
+"""
+)
+
+download_data() # Prompt user to redownload data if they wish
