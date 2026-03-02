e563ce653c61a1d9433b5a45c592763d3df2d303
JamsterJava <128173916+JamsterJava@users.noreply.github.com>
2026-03-02 19:08:20 +0000

    refactor: make csvreader use column names instead of index numbers

--- read_data.py
+++ read_data.py
@@ -5,25 +5,18 @@ from typing import Iterable
 
 def read_data(file: Iterable[str]) -> None:
     print(Style.BRIGHT + "Retrieving necessary data from dataset..." + Style.RESET_ALL)
-    csvreader = csv.reader(file, delimiter=',') # Read the CSV
-    rows: list[str] = [] # The rows in the csv file
+    csvreader = csv.DictReader(file, delimiter=',') # Read the CSV
     movie_count: int = 0 # How many movies are in the specified genre
     revenues: list[float] = [] # A list of all the revenues of these movies
     
     for row_num, row in enumerate(csvreader, start=1):
-        print("Type of row:", type(row))
         if not row:
             print(f"Skipping empty row {row_num}")
             continue
-        if 21 < len(row):
-            print(f"Row {row_num}, Column {21}: {row[21]}")
-            if "Action" in row[21]: # Only look at action movies
-                movie_count += 1
-                rows.append(row)
-                revenues.append(row[7])
-        else:
-            print(f"Row {row_num} has only {len(row)} columns, skipping.")
-            break
+
+        if "Action" in row["genres"]: # Only look at action movies
+            movie_count += 1
+            revenues.append(float(row["revenue"]))
     print("Total movies matching criteria:", movie_count)
 
     print(bold("\n-------------------- Retrieved Data --------------------\n"))
