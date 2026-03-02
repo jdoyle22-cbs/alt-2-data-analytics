0246b4579c2064f72f7de205301efaff8f1082eb
James Doyle <jamesdoyle22@student.cbsenniscorthy.ie>
2026-03-02 13:09:10 +0000

    feat: initial code for mode

--- lib.py
+++ lib.py
@@ -1,7 +1,17 @@
-from colorama import Style
+from colorama import Style, Fore, Back
 
 def mode(data: list):
-    print("TBD")
+    stored_values: dict[int, int] = {}
+
+    try:
+        for value in data:
+            current_index = stored_values[value] if stored_values[value] else 0 # Make sure there is a value
+            stored_values.update({ value: current_index })
+
+        max_key = max(stored_values, key=stored_values.get)
+        return max_key
+    except Exception as e:
+        print(error(str(e)))
 
 def median(data: list):
     print("TBD")
@@ -9,5 +19,10 @@ def median(data: list):
 def mean(data: list):
     print("TBD")
 
+# Terminal styling
+
 def bold(message: str):
-    print(Style.BRIGHT + message + Style.RESET_ALL)
+    return(Style.BRIGHT + message + Style.RESET_ALL)
+
+def error(message: str):
+    return(Style.BRIGHT + Back.RED + " " + message + " " + Style.RESET_ALL)
--- read_data.py
+++ read_data.py
@@ -1,12 +1,13 @@
 from colorama import Fore, Back, Style
 import csv
-from lib import bold
+from lib import bold, mode
 
 def read_data(file: File) -> None:
     print(Style.BRIGHT + "Retrieving necessary data from dataset..." + Style.RESET_ALL)
     csvreader = csv.reader(file, delimiter=',') # Read the CSV
     rows = []
     movie_count: int = 0
+    revenues: list = []
     
     for row_num, row in enumerate(csvreader, start=1):
         if not row:
@@ -17,10 +18,12 @@ def read_data(file: File) -> None:
             if "Action" in row[21]: # Only look at action movies
                 movie_count += 1
                 rows.append(row)
+                revenues.append(row[7])
         else:
             print(f"Row {row_num} has only {len(row)} columns, skipping.")
             break
     print("Total movies matching criteria:", movie_count)
 
     print(bold("\n-------------------- Retrieved Data --------------------\n"))
-    print(bold("Total no. of rows:") + " " + str(csvreader.line_num if csvreader.line_num else "Unknown"))
+    print(bold("Total no. of rows:") + " " + str(csvreader.line_num if csvreader.line_num else "Unable to determine"))
+    print(mode(revenues))
